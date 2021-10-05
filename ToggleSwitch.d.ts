88a38ac17b46bfe9f926d44fa85aa4a22032533d
Chung Wei Leong <15154097+chungweileong94@users.noreply.github.com>
2021-10-04 21:01:37 -0700

    Fixed TypeScript definition
    - Fixed style related PropTypes

--- ToggleSwitch.d.ts
+++ ToggleSwitch.d.ts
@@ -1,25 +1,27 @@
-import React from 'react';
+declare module 'toggle-switch-react-native' {
+  import React from 'react';
+  import {ColorValue, StyleProp, TextStyle, ViewStyle} from 'react-native';
 
-export type ToggleSwitchProps = {
-  isOn: boolean;
-  label?: string;
-  onColor?: string;
-  offColor?: string;
-  size?: string;
-  labelStyle?: object | number;
-  labelPosition?: string;
-  thumbOnStyle?: object | number;
-  thumbOffStyle?: object | number;
-  trackOnStyle?: object | number;
-  trackOffStyle?: object | number;
-  onToggle?: (isOn: boolean) => any;
-  icon?: object;
-  disabled?: boolean;
-  animationSpeed?: number;
-  useNativeDriver?: boolean;
-  circleColor?: string;
+  export type ToggleSwitchProps = {
+    isOn: boolean;
+    label?: string;
+    onColor?: ColorValue;
+    offColor?: ColorValue;
+    size?: 'large' | 'medium' | 'small';
+    labelStyle?: StyleProp<TextStyle>;
+    thumbOnStyle?: StyleProp<ViewStyle>;
+    thumbOffStyle?: StyleProp<ViewStyle>;
+    trackOnStyle?: StyleProp<ViewStyle>;
+    trackOffStyle?: StyleProp<ViewStyle>;
+    onToggle?: (isOn: boolean) => any;
+    icon?: React.ReactNode;
+    disabled?: boolean;
+    animationSpeed?: number;
+    useNativeDriver?: boolean;
+    circleColor?: ColorValue;
+  };
+
+  class ToggleSwitch extends React.Component<ToggleSwitchProps> {}
+
+  export default ToggleSwitch;
 }
-
-type ToggleSwitch = React.ComponentType<ToggleSwitchProps>;
-
-export default ToggleSwitch;
